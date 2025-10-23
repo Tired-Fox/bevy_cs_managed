220bca70b5f7460904be3cd04d9d2312deeb281f
Zachary Boehm <zboehm@menard-inc.com>
2025-10-23 17:17:20 -0500

    [feat] add comments to src/bin/simple.rs

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -21,6 +21,9 @@ pub fn awake(world: DeferredWorld, context: HookContext) {
     let script = entity.get::<Script>().unwrap();
 
     let Some(runtime) = world.get_resource::<Runtime>() else { return };
+    //┌─ Lookup the Awake method that has 0 arguments
+    //┆ Note: This will cache the method if found for future calls
+    //└──────────────────────────────────────────┬────────┐
     let Some(awake) = runtime.get_method(script, "Awake", 0) else { return };
     awake.invoke(());
 }
@@ -32,17 +35,43 @@ fn setup_scripts(world: &mut World) {
 }
 
 fn spawn_scripts(mut commands: Commands, mut runtime: ResMut<Runtime>) {
+    //┌─This is the fullname including namespace [namespace.]ClassName
+    //┆  The below example can be referenced with `Scripts.Creatures.Player`
+    //┆
+    //┆  using Engine;
+    //┆  namespace Scripts
+    //┆  {
+    //┆      namespace Creatures
+    //┆      {
+    //┆          class Player
+    //┆          {
+    //┆             public Vector3 Position;
+    //┆          }
+    //┆      }
+    //┆  }
+    //└─────────────────────────┐
     let script = runtime.create("Player").unwrap();
+    //┌─ Set a property on the script class instance
+    //┆
+    //┆ This works the same with fields with `set_field_value`
+    //┆
+    //┆ Warning: The property must have a public setter
+    //┆
+    //┆┌─ Can pass any reference to a value as long as the structure matches
+    //┆┆    the expected type in the C# method parameter
+    //┆└──────────────────────────────────┐
+    //└────┐                              │
     script.set_property_value("Position", &Vector3 { x: 1.2, y: 2.4, z: 3.6 });
 
-    // Can inspect the fields on a script
     println!("Player {{");
+    // MetaData, fields and properties, are cached when the class type is loaded and
+    //    can be used to for reflection
     let metadata = runtime.get_meta_data(&script);
     for field in &metadata.fields {
-        println!("    {} = {:?}", field.name, script.get_field_value::<Vector3>("Position"));
+        println!("    {} = {:?}", field.name, script.get_field_value::<Vector3>(&field.name));
     }
     for prop in &metadata.properties {
-        println!("    {} = {:?}", prop.name, script.get_property_value::<Vector3>("Position"));
+        println!("    {} = {:?}", prop.name, script.get_property_value::<Vector3>(&prop.name));
     }
     println!("}}");
 
@@ -57,6 +86,12 @@ fn update(
     let dt = delta.delta_secs();
 
     for script in &query {
+        //┌─ Lookup the Update method that has 1 arguments
+        //┆
+        //┆ When invoked the arguments can be passed as a single value reference if there is 1 arg
+        //┆     or as a tuple of value references for multiple args.
+        //┆
+        //└──────────────────────────────────────────────┬─────────┐
         if let Some(update) = runtime.get_method(script, "Update", 1) {
             update.invoke(&dt);
         }
@@ -67,6 +102,9 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(CSharpPlugin)
+        // User is given complete control on how the scripts should be called and manipulated
+        //   the crate handles bootstrapping the runtime and managing script references like
+        //   classes, objects, methods, etc.
         .add_systems(PreStartup, setup_scripts)
         .add_systems(Startup, spawn_scripts)
         .add_systems(Update, update)
